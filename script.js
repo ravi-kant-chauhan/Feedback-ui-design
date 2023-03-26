578e1170bebd68f7929636dc84753c742755b329
Ravi <ravkchauhan1aip@gmail.com>
2023-03-26 22:25:17 +0530

    update

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const ratings = document.querySelectorAll('.rating')
 const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
