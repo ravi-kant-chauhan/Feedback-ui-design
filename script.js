a4ab213ea8261cab48ab91482606aff233514606
Ravi <ravkchauhan1aip@gmail.com>
2023-03-26 22:26:50 +0530

    update

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ sendBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
         <i class="fas fa-heart"></i>
         <strong>Thank You!</strong>
-       
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
     `
 })
