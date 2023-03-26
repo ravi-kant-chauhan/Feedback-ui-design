fee9f16ec8d4ab051bcb5489e7db360837802aea
Ravi <ravkchauhan1aip@gmail.com>
2023-03-26 22:26:42 +0530

    update

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 let selectedRating = 'Satisfied'
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+       
+    `
+})
